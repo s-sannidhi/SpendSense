88e7dbdea64f0491491bb8b684ad211369a9df13
Srujan Sannidhi <srujan.sannidhi7@gmail.com>
2025-01-05 19:08:42 -0600

    Initial Commit

---
+++ SpendSense/SpendSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendSenseApp.swift
+//  SpendSense
+//
+//  Created by Srujan Sannidhi on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SpendSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpendSenseTests/SpendSenseTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpendSenseTests.swift
+//  SpendSenseTests
+//
+//  Created by Srujan Sannidhi on 1/5/25.
+//
+
+import Testing
+
+struct SpendSenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
